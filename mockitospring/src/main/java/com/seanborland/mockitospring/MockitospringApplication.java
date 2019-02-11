0542027c8dd0ed15be00325c35379114401fc363
Sean Borland <sborland@rccl.com>
2019-02-11 11:04:18 -0500

    Add mockitospring to repo

---
+++ mockitospring/src/main/java/com/seanborland/mockitospring/MockitospringApplication.java
@@ -0,0 +1,14 @@
+package com.seanborland.mockitospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitospringApplication.class, args);
+    }
+
+}
+
