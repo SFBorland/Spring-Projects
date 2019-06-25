4cc034b974e1d45bf32a2bbd471af57e3436e7d7
Sean Borland <sborland@rccl.com>
2019-06-25 09:18:00 -0400

    Console works

--- embedded-h2/src/main/resources/data.sql
+++ embedded-h2/src/main/resources/data.sql
@@ -1,5 +1,23 @@
-insert into person
-values(1001,'Sean', 'Borland', '1984-02-13');
+DROP TABLE IF EXISTS person;
+
+create table person
+(
+    id        bigint      not null,
+    firstName varchar(25) not null,
+    lastName  varchar(25) not null,
+    dob       date        not null,
+    primary key (id)
+);
 
 insert into person
-values(1002,'Brandon', 'Borland', '2017-05-29');
+values (1001, 'Sean', 'Borland', '1984-02-13'),
+       (1002, 'Brandon', 'Borland', '2017-05-29');
+
+DROP TABLE IF EXISTS billionaires;
+
+CREATE TABLE billionaires (
+                              id INT AUTO_INCREMENT  PRIMARY KEY,
+                              first_name VARCHAR(250) NOT NULL,
+                              last_name VARCHAR(250) NOT NULL,
+                              career VARCHAR(250) DEFAULT NULL
+);
--- embedded-h2/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table person
-(
-   id bigint not null,
-   firstName varchar(25) not null,
-   lastName varchar(25) not null,
-   dob date not null,
-   primary key(id)
-);
