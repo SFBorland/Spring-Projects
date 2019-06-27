050ca526a27622081c4d7b3b8fcfb9bfe815e36f
Sean Borland <sborland@rccl.com>
2019-06-27 19:55:58 -0400

    Added a timeout example

--- reactive-spring-examples/src/main/java/com/seanborland/reactivespringexamples/webclientexamples/WebClientRunner.java
+++ reactive-spring-examples/src/main/java/com/seanborland/reactivespringexamples/webclientexamples/WebClientRunner.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class WebClientRunner {
     
@@ -17,6 +21,19 @@ public class WebClientRunner {
                 .retrieve()
                 .bodyToMono(ErrorResponse.class)
                 .block();
+        
+        log.info("Response from error-message-service: " + errorResponse);
+    }
+    
+    @Test
+    public void basicRestCallUsingWebClientAndBlockAndTimeout() {
+        
+        ErrorResponse errorResponse = WebClient.create("http://shiptst1.mesos.rccl.com")
+                .get()
+                .uri("/error-messages/v1/read/errors?locale=en&errorCode=no.results.found")
+                .retrieve()
+                .bodyToMono(ErrorResponse.class)
+                .block(Duration.ofSeconds(3));
         
         log.info("Response from error-message-service: " + errorResponse);
     }
