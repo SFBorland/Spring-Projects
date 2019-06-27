f72ac5265395c95b85221c02ad986fa5d11eea9f
Sean Borland <sborland@rccl.com>
2019-06-27 15:19:31 -0400

    Sample WebClient call

---
+++ reactive-spring-examples/src/main/java/com/seanborland/reactivespringexamples/webclientexamples/WebClientRunner.java
@@ -0,0 +1,35 @@
+package com.seanborland.reactivespringexamples.webclientexamples;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+public class WebClientRunner {
+    
+    @Test
+    public void basicRestCallUsingWebClientAndBlock() {
+        
+        WebClient webClient = WebClient
+                .builder()
+                .baseUrl("http://shiptst1.mesos.rccl.com")
+                .build();
+        
+        ErrorResponse errorResponse = webClient.get().uri("/error-messages/v1/read/errors?locale=en&errorCode" +
+                "=no.results.found")
+                .retrieve()
+                .bodyToMono(ErrorResponse.class)
+                .block();
+        
+        log.info("Response from error-message-service: " + errorResponse);
+    }
+}
+
+@Data
+class ErrorResponse {
+    
+    String errorCode;
+    String errorTitle;
+    String errorMessage;
+}
