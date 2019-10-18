d918a94ae4764e7ec57aeb49dcca127bf25821d6
Sean Borland <sborland@rccl.com>
2019-10-18 14:13:03 -0400

    Code to check when path and reqBody can both be passed

--- testing-with-webflux/src/main/java/com/seanborland/testingwithwebflux/model/Employee.java
+++ testing-with-webflux/src/main/java/com/seanborland/testingwithwebflux/model/Employee.java
@@ -1,5 +1,11 @@
 package com.seanborland.testingwithwebflux.model;
 
+import lombok.Data;
+
+@Data
 public class Employee {
     
+    private String id;
+    private String name;
+    private String hometown;
 }
--- testing-with-webflux/src/test/java/com/seanborland/testingwithwebflux/controller/EmployeeControllerTest.java
+++ testing-with-webflux/src/test/java/com/seanborland/testingwithwebflux/controller/EmployeeControllerTest.java
@@ -1,77 +1,77 @@
-package com.seanborland.testingwithwebflux.controller;
-
-import com.seanborland.testingwithwebflux.model.Employee;
-import com.seanborland.testingwithwebflux.service.EmployeeService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
-
-import static org.mockito.Mockito.when;
-
-@RunWith(SpringRunner.class)
-//@RunWith(MockitoJUnitRunner.class) //This won't work because this is an IT test and need a bigger context loaded.
-@WebFluxTest(EmployeeController.class)
-public class EmployeeControllerTest {
-    
-    @Autowired
-    WebTestClient webTestClient;
-    
-    @MockBean //Use @MockBean when using @RunWith(SpringRunner.class).
-    private EmployeeService employeeService;
-    
+//package com.seanborland.testingwithwebflux.controller;
+//
+//import com.seanborland.testingwithwebflux.model.Employee;
+//import com.seanborland.testingwithwebflux.service.EmployeeService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.reactive.server.WebTestClient;
+//import reactor.core.publisher.Mono;
+//
+//import static org.mockito.Mockito.when;
+//
+//@RunWith(SpringRunner.class)
+////@RunWith(MockitoJUnitRunner.class) //This won't work because this is an IT test and need a bigger context loaded.
+//@WebFluxTest(EmployeeController.class)
+//public class EmployeeControllerTest {
+//
+//    @Autowired
+//    WebTestClient webTestClient;
+//
+//    @MockBean //Use @MockBean when using @RunWith(SpringRunner.class).
+//    private EmployeeService employeeService;
+//
+////    @Test
+////    public void testGetEmployeeById() {
+////        Employee employee = Employee.builder().id(1).city("delhi").age(23).name("ABC").build();
+////        Mono<Employee> employeeMono = Mono.just(employee);
+////
+////        when(employeeService.getEmployeeById(1)).thenReturn(employeeMono);
+////
+////        webTestClient.get()
+////                .uri("/employees/1")
+////                .accept(MediaType.APPLICATION_JSON)
+////                .exchange()
+////                .expectStatus().isOk()
+////                .expectBody(Employee.class)
+////                .value(employee1 -> employee.getAge(), equalTo(23));
+////    }
+//
 //    @Test
-//    public void testGetEmployeeById() {
-//        Employee employee = Employee.builder().id(1).city("delhi").age(23).name("ABC").build();
-//        Mono<Employee> employeeMono = Mono.just(employee);
+//    public void testDeleteEmployeeById() {
 //
-//        when(employeeService.getEmployeeById(1)).thenReturn(employeeMono);
+//        when(employeeService.deleteEmployeeById(1)).thenReturn(Mono.just("Employee with id 1 is deleted."));
 //
-//        webTestClient.get()
+//        webTestClient.delete()
 //                .uri("/employees/1")
-//                .accept(MediaType.APPLICATION_JSON)
 //                .exchange()
 //                .expectStatus().isOk()
-//                .expectBody(Employee.class)
-//                .value(employee1 -> employee.getAge(), equalTo(23));
-//    }
-    
-    @Test
-    public void testDeleteEmployeeById() {
-        
-        when(employeeService.deleteEmployeeById(1)).thenReturn(Mono.just("Employee with id 1 is deleted."));
-        
-        webTestClient.delete()
-                .uri("/employees/1")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .isEqualTo("Employee with id 1 is deleted.");
-        
-    }
-    
-//    @Test
-//    public void testCreateEmployee() {
-//
-//        Employee employee = Employee.builder().id(1).city("delhi").age(23).name("ABC").build();
-//        Mono<Employee> employeeMono = Mono.just(employee);
-//        when(employeeService.createEmployee(employee)).thenReturn(employeeMono);
-//
-//        webTestClient.post()
-//                .uri("/employees")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON)
-//                .body(Mono.just(employee), Employee.class)
-//                .exchange()
-//                .expectStatus().isCreated();
+//                .expectBody(String.class)
+//                .isEqualTo("Employee with id 1 is deleted.");
 //
 //    }
-}
+//
+////    @Test
+////    public void testCreateEmployee() {
+////
+////        Employee employee = Employee.builder().id(1).city("delhi").age(23).name("ABC").build();
+////        Mono<Employee> employeeMono = Mono.just(employee);
+////        when(employeeService.createEmployee(employee)).thenReturn(employeeMono);
+////
+////        webTestClient.post()
+////                .uri("/employees")
+////                .contentType(MediaType.APPLICATION_JSON)
+////                .accept(MediaType.APPLICATION_JSON)
+////                .body(Mono.just(employee), Employee.class)
+////                .exchange()
+////                .expectStatus().isCreated();
+////
+////    }
+//}
