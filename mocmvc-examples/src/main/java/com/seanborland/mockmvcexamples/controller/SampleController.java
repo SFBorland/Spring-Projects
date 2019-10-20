c0cc9d2514ce61127128608b3d7cb296330c35fc
Sean <seanfborland@gmail.com>
2019-10-20 16:54:00 -0400

    mockmvc-example

--- mocmvc-examples/src/main/java/com/seanborland/mockmvcexamples/controller/SampleController.java
+++ mocmvc-examples/src/main/java/com/seanborland/mockmvcexamples/controller/SampleController.java
@@ -2,13 +2,16 @@ package com.seanborland.mockmvcexamples.controller;
 
 import com.seanborland.mockmvcexamples.model.Player;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping
 public class SampleController {
     
     @GetMapping("/getPlayer")
-    private Player getPlayer() {
+    public Player getPlayer() {
+        
         return new Player("Sean Borland", "Manchester City", 8);
     }
 }
--- mocmvc-examples/src/test/java/com/seanborland/mockmvcexamples/controller/SampleControllerTest.java
+++ mocmvc-examples/src/test/java/com/seanborland/mockmvcexamples/controller/SampleControllerTest.java
@@ -1,27 +1,44 @@
 package com.seanborland.mockmvcexamples.controller;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+@WebMvcTest
+@AutoConfigureMockMvc
 class SampleControllerTest {
     
-    @InjectMocks
-    SampleController sampleController;
+    @Autowired
+    MockMvc mockMvc;
     
-    private MockMvc mockMvc;
+    @Before
+    public void setUp() {
     
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(sampleController)
-                .build();
     }
     
+    @Test
+    public void alpha() throws Exception {
     
-    public void alpha() {
+//        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(sampleController)
+//                .build();
+        
         mockMvc.perform(MockMvcRequestBuilders.get("/getPlayer"))
-                .andExpect(MockM)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("Sean Borland")));
     }
 }
