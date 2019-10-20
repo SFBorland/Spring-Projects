690fde9bf3151877c1c0fed9da731a2cb758a91d
Sean <seanfborland@gmail.com>
2019-10-20 17:07:17 -0400

    Added wroking minimal example

--- mocmvc-examples/src/test/java/com/seanborland/mockmvcexamples/controller/SampleControllerTest.java
+++ mocmvc-examples/src/test/java/com/seanborland/mockmvcexamples/controller/SampleControllerTest.java
@@ -2,20 +2,15 @@ package com.seanborland.mockmvcexamples.controller;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest
@@ -33,7 +28,7 @@ class SampleControllerTest {
     
     @Test
     public void alpha() throws Exception {
-    
+
 //        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(sampleController)
 //                .build();
         
