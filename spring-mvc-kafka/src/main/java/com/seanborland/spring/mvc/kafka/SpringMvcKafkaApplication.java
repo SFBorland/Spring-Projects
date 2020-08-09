585bae4ea88a8d446ca1560e05e9dcceb431c8c3
Sean Borland <sborland@rccl.com>
2020-08-09 08:56:25 -0400

    initial commit

---
+++ spring-mvc-kafka/src/main/java/com/seanborland/spring/mvc/kafka/SpringMvcKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.seanborland.spring.mvc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcKafkaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcKafkaApplication.class, args);
+    }
+    
+}
