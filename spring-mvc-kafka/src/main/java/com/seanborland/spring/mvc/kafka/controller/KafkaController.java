0554fc0287ce69ae4cdf9066e221850409840493
Sean Borland <sborland@rccl.com>
2020-08-09 09:02:13 -0400

    Created initial packages

---
+++ spring-mvc-kafka/src/main/java/com/seanborland/spring/mvc/kafka/controller/KafkaController.java
@@ -0,0 +1,5 @@
+package com.seanborland.spring.mvc.kafka.controller;
+
+public class KafkaController {
+
+}
---
+++ spring-mvc-kafka/src/main/java/com/seanborland/spring/mvc/kafka/service/GenericKafkaProducer.java
@@ -0,0 +1,5 @@
+package com.seanborland.spring.mvc.kafka.service;
+
+public class GenericKafkaProducer {
+
+}
