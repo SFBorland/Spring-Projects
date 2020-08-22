94cbd134ef3e44b1709a2e4612cab48a2d24d876
Sean Borland <sborland@rccl.com>
2020-08-22 08:02:35 -0400

    Added @Primary example

--- spring-sample-no-xml/src/main/java/com/seanborland/repository/HibernateCustomerRepositoryImpl.java
+++ spring-sample-no-xml/src/main/java/com/seanborland/repository/HibernateCustomerRepositoryImpl.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import com.seanborland.model.Customer;
 
-//** Stereotyping: This annotation serves as a specialization of @Component, allowing for implementation classes to be 
+//Since there are 3 beans that implement CustomerRepository, this tells Spring to always use this one.
+@Primary
+//** Stereotyping: This annotation serves as a specialization of @Component, allowing for implementation classes to be
 //auto-detected through classpath scanning.
-@Repository("customerRepository")
+@Repository("hibernateRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
     
     //** This is for the app.properties. The value in the prop file gets substituted here.
